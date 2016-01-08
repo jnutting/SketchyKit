d24177bc05215d6a8b5e8b64e4a18d913077f366
Jack Nutting <jacknutting@mac.com>
2016-01-08 09:41:11 +0100

    Initial Commit

---
+++ SketchyUIKit/SketchyUIKit.h
@@ -0,0 +1,19 @@
+//
+//  SketchyUIKit.h
+//  SketchyUIKit
+//
+//  Created by JN on 2016-1-8.
+//  Copyright © 2016 thoughtbot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SketchyUIKit.
+FOUNDATION_EXPORT double SketchyUIKitVersionNumber;
+
+//! Project version string for SketchyUIKit.
+FOUNDATION_EXPORT const unsigned char SketchyUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SketchyUIKit/PublicHeader.h>
+
+
